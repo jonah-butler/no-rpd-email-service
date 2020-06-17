44de0836859cf7459fa56f14b2561fb55ae0bf69
Jonah Butler <jonahbutler6@gmail.com>
2020-06-17 14:00:42 -0400

    added procfile for heroku and updated app.js with .env port variable

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ app.post("/submit", (req, res) => {
 
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
 app.listen(3000, (req, res) => {
 	console.log('server is running');
 })
